76d118ee8c62d01bd19b6f7a549c622ac66b760f
vasil87 <vasil.kamburov@abv.bg>
2016-07-03 20:28:04 +0300

    GlobalConstants

---
+++ Global/GlobalConstants.cs
@@ -0,0 +1,20 @@
+﻿
+namespace OtbornaIgra.Global
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class GlobalConstants
+    {
+        public const int distanceFromBottomRowPad = 5; //na kakvo razstoqnie da e pada ot dunoto  
+        public const int padWidth = 150;
+        public const int padHeight = 15;
+        public const int ballSize = 30;
+        public const int brickWidth = ballSize * 2;
+        public const int brickHright = 20;
+        public const double timerFramesIntervalInMiliSeconds = 10; //tova e speeda na igrata
+    }
+}
